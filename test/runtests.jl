b4f134e34d2418c7fe61cc2a316125dc22ce0535
IlianPihlajamaa <ilian.pihlajamaa@gmail.com>
2025-11-11 11:37:17 +0100

    change tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,24 +2,27 @@ using TestPackage
 using Test, Random
 @show Random.GLOBAL_SEED
 
+function foo(n)
+    println("Starting to process $n floats...")
+    buffer = 0
+    @time for i in 1:n
+        # Create a random float
+        x = rand() * 100.0
+        # Round it and convert to Int128
+        rounded_val = ceil(Int, x)
+        # Add to buffer
+        buffer += rounded_val
+    end
+    return buffer
+end
+
 
 @testset "TestPackage.jl" begin
     # Create a billion floats in a loop, round them, and add to buffer
     @testset "Billion floats test" begin
-        buffer = 0
-        n = 1_000_000_000  
-        
-        println("Starting to process $n floats...")
-        
-        @time for i in 1:n
-            # Create a random float
-            x = rand() * 100.0
-            # Round it and convert to Int128
-            rounded_val = ceil(Int, x)
-            # Add to buffer
-            buffer += rounded_val
-        end
-        
+        n = 1_000_000_000
+
+        buffer = foo(n)
         println("Final buffer value: $buffer")
         
         # Basic sanity check - buffer should be positive and non-zero
